e5512478dc421c9d8d20f655e7e7832a1411644a
darshil89 <darshilmahraur67@gmail.com>
2023-08-01 22:08:49 +0530

    movie part done

---
+++ src/app/components/MovieCard.js
@@ -0,0 +1,30 @@
+import styles from "@/app/styles/common.module.css";
+import Image from "next/image";
+import Link from "next/link";
+const MovieCard = (item) => {
+  const { id, title, type, synopsis } = item.jawSummary;
+  //   console.log(item.jawSummary.backgroungImage);
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          <Image
+            src={item.jawSummary.backgroundImage.url}
+            alt={title}
+            width={260}
+            height={200}
+          />
+        </div>
+        <div className={styles.card_data}>
+          <h1 className={styles.card_title}>{title.substring(0, 18)}</h1>
+          <p>{synopsis.substring(0, 100)} ...</p>
+          <Link href={`/movie/${id}`}>
+            <button>Read More</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieCard;
--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css";
+const Movie = async () => {
+  const url = process.env.RAPID_KEY;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "967347ae15msh67d18f76af58303p173f0ajsn82a0b7b70dae",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
 
-const Movie = () => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+  // console.log(data);
+  const main_data = data.titles;
   return (
-    <h1>
-      Movie Page
-    </h1>
-  )
-}
+    <>
+      <section className={styles.movieSection}>
+        <div className={styles.container}>
+          <h1 >Series and Movie</h1>
+          <div className={styles.card_section}>
+            {main_data.map((item) => {
+              return <MovieCard key={item.id} {...item} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Movie
+export default Movie;
